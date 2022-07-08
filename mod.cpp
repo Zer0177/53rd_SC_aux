52f32659343bec18c67efe4c1de42e09f44027d8
Seven <jamesmehew@yahoo.ca>
2022-07-08 14:59:03 -0400

    mod.cpp and logo
    for body to move into the correct folder

--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,4 @@
 name = "53rd Aux Mod";
-picture = "53rd_SC_aux\Hephaestus128x128.paa";
-logo = "53rd_SC_aux\Hephaestus128x128.paa";
-logoSmall = "53rd_SC_aux\Hephaestus64x64.paa";
-logoOver = "53rd_SC_aux\Hephaestus128x128.paa";
+logo = "Hephaestus.paa";
 description = "Don't Want to Fuck You?";
-tooltipOwned="53rd Strike Company";
-hideName = 0;
-hidePicture = 0;
 author = "53rd Mod Team"
-overviewPicture = "53rd_SC_aux\Hephaestus128x128.paa";
-
