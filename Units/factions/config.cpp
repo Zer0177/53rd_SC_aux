60b604ea87f9eaef05483236c01b13a55cb7a4b9
Seven <jamesmehew@yahoo.ca>
2022-07-17 23:47:26 -0400

    Revert "Factions"
    This reverts commit a0bb5d2729f664c71ff390dd9ecf9bf9fa71486c.

--- Units/factions/config.cpp
+++ Units/factions/config.cpp
@@ -20,8 +20,11 @@ class CfgFactionClasses
 	class 53rd_faction
 	{
         displayName="[53rd] Strike Company";
-        priority=1;
+		icon="\OPTRE_Core\data\icon_UNSC_ca.paa";
+		flag="\OPTRE_Core\Data\flag_UNSC_ca.paa";
+		priority=1;
 		side=1;
+		primaryLanguage="EN";
 	};
 };
 class CfgEditorCategories
@@ -29,8 +32,6 @@ class CfgEditorCategories
 	class 53rd_cat_faction
 	{ 
         displayName="[53rd] Strike Company";
-        priority=1;
-		side=1;
 	};
 };
 class CfgEditorSubcategories
@@ -38,60 +39,34 @@ class CfgEditorSubcategories
 	class 53rd_Men_Hephaestus
 	{
 		displayName="Men (Hephaestus)";
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator=2;
-        priority=1;
         
 	};
 	class 53rd_Men_Athena
 	{
 		displayName="Men (Athena)";
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator=2;
-        priority=1;
 	};
 	class 53rd_Tank
 	{
 		displayName="Tanks";
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator=2;
 	};
 	class 53rd_FixedWing
 	{
 		displayName="Fixed Wing";
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator=2;
 	};
 	class 53rd_IFV
 	{
 		displayName="IFVs";
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator=2;
 	};
 	class 53rd_Rotary
 	{
 		displayName="Rotary";
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator=2;
 	};
 	class 53rd_APC
 	{
 		displayName="APC";
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator=2;
 	};
 	class 53rd_Motor
 	{
 		displayName="Motorized";
-		scope = 2;
-		scopeArsenal = 2;
-		scopeCurator=2;
 	};
 };
