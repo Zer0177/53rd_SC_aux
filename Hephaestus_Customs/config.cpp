20bd505b3e451276980ab8c72a861418ddb3a113
Ithias <ALDieguez9@gmail.com>
2023-01-10 06:53:03 -0600

    Custom armor is back
    Still needs the Xtd, but we gucci

--- Hephaestus_Customs/config.cpp
+++ Hephaestus_Customs/config.cpp
@@ -14,7 +14,7 @@ class CfgPatches {
 		requiredAddons[] = {};
 	};
 };
-#include "xtdGear.hpp"
+//#include "xtdGear.hpp"
 class cfgWeapons 
 {
     class ItemInfo;
@@ -35,10 +35,14 @@ class cfgWeapons
 	class 53rd_Armor_Medic;
 	class 53rd_Armor_JTAC;
 	class 53rd_Sealed_Base;
+	class 53rd_Sealed_Base_dp;
 	class 53rd_Unsealed_Base;
+	class 53rd_Unsealed_Base_dp;
 	class 53rd_Hephaestus_BDU_Slim;
 
 //Customs
+
+//Fixer
 
 class 53rd_Custom_Fixer : 53rd_Armor_SL
 	{
@@ -118,6 +122,615 @@ class 53rd_Custom_Fixer : 53rd_Armor_SL
 			};
 		};
     };
+
+//Body
+
+class 53rd_Custom_Armor_Body_Desert : 53rd_Armor_SL
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Body / Desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Desert.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Body_Desert.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Desert.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Body_Snow : 53rd_Armor_SL
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Body / Snow)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Snow.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Body_Snow.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Snow.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Body_Urban : 53rd_Armor_SL
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Body / Urban)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Urban.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Body_Urban.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Urban.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Body_Woodland : 53rd_Armor_SL
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Body / Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Woodland.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Body_Woodland.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Woodland.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+
+//Panzer
+
+class 53rd_Custom_Base_Armor_Panzer: 53rd_Armor_Rifleman
+	{
+		author="Article 2 Studios";
+		picture="\OPTRE_UNSC_Units\Army\icons\Army_vest_wdl.paa";
+		displayName="M52A Body Armor (Heavy) [Woodland]";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				//"A_Base",
+				//"A_ChestArmor",
+				"A_Ghillie",
+				//"A_KneesLeft",
+				//"A_KneesRight",
+				"A_KneesMarLeft",
+				"A_KneesMarRight",
+				"A_ODST",
+				//"A_ShinArmorLeft",
+				//"A_ShinArmorRight",
+				"A_TacPad",
+				//"A_ThighArmorLeft",
+				//"A_ThighArmorRight",
+				//"AS_BaseLeft",
+				//"AS_BaseRight",
+				//"AS_LargeLeft",
+				"AS_LargeRight",
+				"AS_MediumLeft",
+				//"AS_MediumRight",
+				"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+				"AS_ODSTLeft",
+				"AS_ODSTRight",
+				"AS_ODSTSniperLeft",
+				"AS_ODSTSniperRight",
+				"AS_SmallLeft",
+				"AS_SmallRight",
+				//"AP_AR",
+				"AP_BR",
+				//"AP_Canteen",
+				"AP_GL",
+				//"AP_Knife",
+				//"AP_MGThigh",
+				//"AP_AR",
+				//"AP_Pack",
+				"AP_Pistol",
+				"AP_Rounds",
+				"AP_SG",
+				"AP_SMG",
+				"AP_Sniper",
+				//"AP_Thigh",
+				//"AP_Frag",
+				"AP_Smoke",
+				"APO_AR",
+				"APO_BR",
+				"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+		};
+	};
+class 53rd_Custom_Armor_Panzer_Desert : 53rd_Custom_Base_Armor_Panzer
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Panzer / Desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Desert.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Panzer_Desert.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Desert.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Panzer_Snow : 53rd_Custom_Base_Armor_Panzer
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Panzer / Snow)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Snow.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Panzer_Snow.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Snow.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Panzer_Urban : 53rd_Custom_Base_Armor_Panzer
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Panzer / Urban)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Urban.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Panzer_Urban.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Urban.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Panzer_Woodland : 53rd_Custom_Base_Armor_Panzer
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Panzer / Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Woodland.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Panzer_Woodland.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Woodland.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Helmet_Vac_Sealed_Panzer_Desert : 53rd_Sealed_Base
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Panzer / Desert)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Panzer_Desert.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Visor_Gold.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Vac_Sealed_Panzer_Desert_dp : 53rd_Sealed_Base_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Panzer / Desert)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Panzer_Desert.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+    };
+class 53rd_Custom_Helmet_Vac_Sealed_Panzer_Snow : 53rd_Sealed_Base
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Panzer / Snow)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Panzer_Snow.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Visor_Gold.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Vac_Sealed_Panzer_Snow_dp : 53rd_Sealed_Base_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Panzer / Snow)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Panzer_Snow.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+    };
+class 53rd_Custom_Helmet_Vac_Sealed_Panzer_Urban : 53rd_Sealed_Base
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Panzer / Urban)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Panzer_Urban.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Visor_Gold.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Vac_Sealed_Panzer_Urban_dp : 53rd_Sealed_Base_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Panzer / Urban)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Panzer_Urban.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+    };
+class 53rd_Custom_Helmet_Vac_Sealed_Panzer_Woodland : 53rd_Sealed_Base
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Panzer / Woodland)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Panzer_Woodland.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Visor_Gold.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Vac_Sealed_Panzer_Woodland_dp : 53rd_Sealed_Base_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Panzer / Woodland)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Panzer_Woodland.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+    };
+
+//Ryuu
+
+class 53rd_Custom_Armor_Ryuu_Desert : 53rd_Armor_SL
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Ryuu / Desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Desert.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Ryuu_Desert.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Desert.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Ryuu_Snow : 53rd_Armor_SL
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Ryuu / Snow)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Snow.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Ryuu_Snow.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Snow.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Ryuu_Urban : 53rd_Armor_SL
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Ryuu / Urban)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Urban.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Ryuu_Urban.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Urban.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Ryuu_Woodland : 53rd_Armor_SL
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Ryuu / Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Woodland.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Ryuu_Woodland.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Woodland.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+
+//Ithias
+
+class 53rd_Custom_Base_Armor_Ithias: 53rd_Armor_Rifleman
+	{
+		author="Article 2 Studios";
+		picture="\OPTRE_UNSC_Units\Army\icons\Army_vest_wdl.paa";
+		displayName="M52A Body Armor (Heavy) [Woodland]";
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				//"A_Base",
+				//"A_ChestArmor",
+				"A_Ghillie",
+				//"A_KneesLeft",
+				//"A_KneesRight",
+				"A_KneesMarLeft",
+				"A_KneesMarRight",
+				"A_ODST",
+				//"A_ShinArmorLeft",
+				//"A_ShinArmorRight",
+				"A_TacPad",
+				//"A_ThighArmorLeft",
+				//"A_ThighArmorRight",
+				//"AS_BaseLeft",
+				//"AS_BaseRight",
+				//"AS_LargeLeft",
+				"AS_LargeRight",
+				"AS_MediumLeft",
+				"AS_MediumRight",
+				"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+				"AS_ODSTLeft",
+				"AS_ODSTRight",
+				"AS_ODSTSniperLeft",
+				"AS_ODSTSniperRight",
+				"AS_SmallLeft",
+				"AS_SmallRight",
+				//"AP_AR",
+				"AP_BR",
+				"AP_Canteen",
+				"AP_GL",
+				//"AP_Knife",
+				"AP_MGThigh",
+				//"AP_AR",
+				//"AP_Pack",
+				"AP_Pistol",
+				//"AP_Rounds"
+				"AP_SG",
+				"AP_SMG",
+				"AP_Sniper",
+				"AP_Thigh",
+				//"AP_Frag",
+				//"AP_Smoke",
+				"APO_AR",
+				"APO_BR",
+				"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+		};
+	};
+class 53rd_Custom_Armor_Ithias_Desert : 53rd_Custom_Base_Armor_Ithias
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Ithias / Desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Desert.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Ithias_Desert.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Desert.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Ithias_Snow : 53rd_Custom_Base_Armor_Ithias
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Ithias / Snow)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Snow.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Ithias_Snow.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Snow.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Ithias_Urban : 53rd_Custom_Base_Armor_Ithias
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Ithias / Urban)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Urban.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Ithias_Urban.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Urban.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Armor_Ithias_Woodland : 53rd_Custom_Base_Armor_Ithias
+	{ 
+		scope = 2;
+        scopeCurator = 2;
+        scopeArsenal = 2;
+		dlc = "53rd AUX";
+		author = "53rd AUX Team";
+		displayName="[53rd] Custom Armor (Ithias / Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Vest_Woodland.paa",
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Armor_Ithias_Woodland.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Leg_Woodland.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"optre_unsc_units\army\data\odst_armor_co.paa"
+		};
+	};
+class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Desert : 53rd_Sealed_Base
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Ithias / Desert)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Ithias_Desert.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Visor_Gold.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Desert_dp : 53rd_Sealed_Base_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Ithias / Desert)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Ithias_Desert.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+    };
+class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Snow : 53rd_Sealed_Base
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Ithias / Snow)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Ithias_Snow.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Visor_Gold.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Snow_dp : 53rd_Sealed_Base_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Ithias / Snow)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Ithias_Snow.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+    };
+class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Urban : 53rd_Sealed_Base
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Ithias / Urban)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Ithias_Urban.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Visor_Gold.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Urban_dp : 53rd_Sealed_Base_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Ithias / Urban)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Ithias_Urban.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+    };
+class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Woodland : 53rd_Sealed_Base
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Ithias / Woodland)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Ithias_Woodland.paa",
+			"53rd_SC_aux\tex\Hephaestus Armor\53rd_Hephaestus_Visor_Gold.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Woodland_dp : 53rd_Sealed_Base_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Vac Sealed Helmet (Ithias / Woodland)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\53rd_Custom_Helmet_Ithias_Woodland.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+    };
+
 };
 
 class CfgVehicles
