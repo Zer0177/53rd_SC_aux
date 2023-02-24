fbf4f9da1762deeea46ba4066ed17a1f37227a5c
Seven <jamesmehew@yahoo.ca>
2023-02-24 14:36:51 -0500

    Short sleeve

--- Uniforms/config.cpp
+++ Uniforms/config.cpp
@@ -19,6 +19,7 @@ class cfgWeapons
     class UniformItem;
 	class VES_BDU_Evolved_Slim;
 	class VES_BDU_R_Evolved_Slim;
+	class VES_BDU_SS_Evolved_Slim;
 
 //Uniform
 class 53rd_Hephaestus_BDU_Slim: VES_BDU_Evolved_Slim
@@ -55,6 +56,23 @@ class 53rd_Hephaestus_BDU_R_Slim: VES_BDU_R_Evolved_Slim
 			uniformClass="53rd_Hephaestus_BDU_R_V_Slim";
 		};
 	};
+class 53rd_Hephaestus_BDU_SS_Slim: VES_BDU_SS_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform Short Sleeve";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_SS_V_Slim";
+		};
+	};
 };
 
 
@@ -62,6 +80,7 @@ class CfgVehicles
 {
 class VES_BDU_V_Evolved_Slim;
 class VES_BDU_R_V_Evolved_Slim;
+class VES_BDU_SS_V_Evolved_Slim;
 
 	
 class 53rd_Hephaestus_BDU_V_Slim: VES_BDU_V_Evolved_Slim
@@ -78,6 +97,16 @@ class 53rd_Hephaestus_BDU_R_V_Slim: VES_BDU_R_V_Evolved_Slim
 	{
 		scope=1;
         uniformclass="53rd_Hephaestus_BDU_R_Slim";
+		hiddenSelectionsTextures[] = 
+        {
+            "V_FZ_Armor\Data\Uniforms\V_BDU_R_Evolved_CO",
+            "V_FZ_Armor\Data\Uniforms\V_BDU_R_Evolved_CO"
+        };
+	};
+class 53rd_Hephaestus_BDU_SS_V_Slim: VES_BDU_SS_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_SS_Slim";
 		hiddenSelectionsTextures[] = 
         {
             "V_FZ_Armor\Data\Uniforms\V_BDU_R_Evolved_CO",
