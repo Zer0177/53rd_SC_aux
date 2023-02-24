f6d46537bbd71c81b51c0e54a41a83150a4a5c7b
Andrew Dean <bodybagfilla@gmail.com>
2023-02-24 18:04:59 -0500

    Scorpion and Hazard Temp

--- Hephaestus_Customs/config.cpp
+++ Hephaestus_Customs/config.cpp
@@ -970,6 +970,31 @@ class 53rd_Custom_Helmet_Vac_Sealed_Ithias_Woodland : 53rd_Sealed_Base
 class OPTRE_UNSC_CH252D_Helmet;
 class OPTRE_UNSC_CH252D_Helmet_dp;
     
+class 53rd_Custom_Helmet_Custom_Urban : OPTRE_UNSC_CH252D_Helmet
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Helmet (Lonewulf / Urban)";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\odst_Custom_woodland_co.paa",
+			"OPTRE_UNSC_Units\Army\data\ODST_Helmet_Visor_co.paa"
+		};
+    };
+	class 53rd_Custom_Helmet_Custom_Urban_dp : OPTRE_UNSC_CH252D_Helmet_dp
+    {
+        scope = 2;
+        scopeArsenal = 2;
+        displayName = "[53rd] Custom Helmet (Lonewulf / Urban) dp";
+        author = "53rd AUX TEAM";
+        hiddenSelectionsTextures[] = 
+        {
+			"53rd_SC_aux\tex\Customs\odst_Custom_woodland_co.paa",
+			"OPTRE_UNSC_Units\Army\data\ODST_Helmet_Visor_ca.paa"
+		};
+    };
+    
 class 53rd_Custom_Helmet_Lonewulf_Urban : OPTRE_UNSC_CH252D_Helmet
     {
         scope = 2;
--- Vehicles/Scorpion/config.cpp
+++ Vehicles/Scorpion/config.cpp
@@ -22,10 +22,10 @@ class OPTRE_M808BM_UNSC;
 		scopeArsenal=2;
 		editorCategory = "53rd_cat_faction";
 		editorSubCategory = "53rd_Tank";
-		crew="OPTRE_UNSC_Army_Soldier_Crewman_DES";
+		crew="OPTRE_UNSC_ONI_Soldier_Operative";
 		typicalCargo[]=
 		{
-			"OPTRE_UNSC_Army_Soldier_Crewman_DES"
+			"OPTRE_UNSC_ONI_Soldier_Operative"
 		};
 		weapons[]=
 		{
