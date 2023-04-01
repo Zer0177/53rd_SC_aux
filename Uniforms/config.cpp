e5969d1db077676af0f4350384657d5bc8332dda
Ithias <ALDieguez9@gmail.com>
2023-04-01 03:50:01 -0500

    Camo Uniforms
    I need to fix there arsenal pic and add ace extended to em

--- Uniforms/config.cpp
+++ Uniforms/config.cpp
@@ -21,7 +21,7 @@ class cfgWeapons
 	class VES_BDU_R_Evolved_Slim;
 	class VES_BDU_SS_Evolved_Slim;
 
-//Uniform
+// Urban Uniform
 class 53rd_Hephaestus_BDU_Slim: VES_BDU_Evolved_Slim
     {
         dlc="53rd";
@@ -73,7 +73,164 @@ class 53rd_Hephaestus_BDU_SS_Slim: VES_BDU_SS_Evolved_Slim
 			uniformClass="53rd_Hephaestus_BDU_SS_V_Slim";
 		};
 	};
+// Desert Uniform
+class 53rd_Hephaestus_BDU_Slim_Desert: VES_BDU_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform (Desert)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_V_Slim_Desert";
+		};
+	};
+class 53rd_Hephaestus_BDU_R_Slim_Desert: VES_BDU_R_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform Rolled (Desert)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_R_V_Slim_Desert";
+		};
+	};
+class 53rd_Hephaestus_BDU_SS_Slim_Desert: VES_BDU_SS_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform Short Sleeve (Desert)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_SS_V_Slim_Desert";
+		};
+	};
+// Snow Uniform
+class 53rd_Hephaestus_BDU_Slim_Snow: VES_BDU_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform (Snow)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_V_Slim_Snow";
+		};
+	};
+class 53rd_Hephaestus_BDU_R_Slim_Snow: VES_BDU_R_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform Rolled (Snow)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_R_V_Slim_Snow";
+		};
+	};
+class 53rd_Hephaestus_BDU_SS_Slim_Snow: VES_BDU_SS_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform Short Sleeve (Snow)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_SS_V_Slim_Snow";
+		};
+	};
+// Woodland Uniform
+class 53rd_Hephaestus_BDU_Slim_Woodland: VES_BDU_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform (Woodland)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_V_Slim_Woodland";
+		};
+	};
+class 53rd_Hephaestus_BDU_R_Slim_Woodland: VES_BDU_R_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform Rolled (Woodland)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_R_V_Slim_Woodland";
+		};
+	};
+class 53rd_Hephaestus_BDU_SS_Slim_Woodland: VES_BDU_SS_Evolved_Slim
+    {
+        dlc="53rd";
+		scope=2;
+		author="Ithias";
+		displayName="[53rd] Combat Uniform Short Sleeve (Woodland)";
+		picture="\optre_unsc_units\army\icons\army_uniform";
+		CBRN_protection=1;
+		class ItemInfo: UniformItem
+		{
+			containerClass="Supply120";
+			mass=40;
+			uniformType = "Neopren";
+			uniformModel="";
+			uniformClass="53rd_Hephaestus_BDU_SS_V_Slim_Woodland";
+		};
+	};
 };
+
 
 
 class CfgVehicles
@@ -82,7 +239,7 @@ class VES_BDU_V_Evolved_Slim;
 class VES_BDU_R_V_Evolved_Slim;
 class VES_BDU_SS_V_Evolved_Slim;
 
-	
+//Urban Uniform
 class 53rd_Hephaestus_BDU_V_Slim: VES_BDU_V_Evolved_Slim
 	{
 		scope=1;
@@ -111,6 +268,99 @@ class 53rd_Hephaestus_BDU_SS_V_Slim: VES_BDU_SS_V_Evolved_Slim
         {
             "V_FZ_Armor\Data\Uniforms\V_BDU_R_Evolved_CO",
             "V_FZ_Armor\Data\Uniforms\V_BDU_R_Evolved_CO"
+        };
+	};
+//Desert Uniform
+class 53rd_Hephaestus_BDU_V_Slim_Desert: VES_BDU_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_Slim_Desert";
+		hiddenSelectionsTextures[]=
+		{
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-DES2.paa",
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-DES2.paa"
+		};
+	};
+class 53rd_Hephaestus_BDU_R_V_Slim_Desert: VES_BDU_R_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_R_Slim_Desert";
+		hiddenSelectionsTextures[] = 
+        {
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-DES2.paa",
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-DES2.paa"
+        };
+	};
+class 53rd_Hephaestus_BDU_SS_V_Slim_Desert: VES_BDU_SS_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_SS_Slim_Desert";
+		hiddenSelectionsTextures[] = 
+        {
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-DES2.paa",
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-DES2.paa"
+        };
+	};
+//Snow Uniform
+class 53rd_Hephaestus_BDU_V_Slim_Snow: VES_BDU_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_Slim_Snow";
+		hiddenSelectionsTextures[]=
+		{
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-SNO.paa",
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-SNO.paa"
+		};
+	};
+class 53rd_Hephaestus_BDU_R_V_Slim_Snow: VES_BDU_R_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_R_Slim_Snow";
+		hiddenSelectionsTextures[] = 
+        {
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-SNO.paa",
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-SNO.paa"
+        };
+	};
+class 53rd_Hephaestus_BDU_SS_V_Slim_Snow: VES_BDU_SS_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_SS_Slim_Snow";
+		hiddenSelectionsTextures[] = 
+        {
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-SNO.paa",
+			"TCF_EQUIPMENT\Uniforms\BDU\data\UNSC-A-SNO.paa"
+        };
+	};
+//Woodland Uniform
+class 53rd_Hephaestus_BDU_V_Slim_Woodland: VES_BDU_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_Slim_Woodland";
+		hiddenSelectionsTextures[]=
+		{
+			"V_FZ_Armor\Data\Uniforms\V_BDU_TTsKOWoodland_CO",
+			"V_FZ_Armor\Data\Uniforms\V_BDU_TTsKOWoodland_CO"
+		};
+	};
+class 53rd_Hephaestus_BDU_R_V_Slim_Woodland: VES_BDU_R_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_R_Slim_Woodland";
+		hiddenSelectionsTextures[] = 
+        {
+			"V_FZ_Armor\Data\Uniforms\V_BDU_TTsKOWoodland_CO",
+			"V_FZ_Armor\Data\Uniforms\V_BDU_TTsKOWoodland_CO"
+        };
+	};
+class 53rd_Hephaestus_BDU_SS_V_Slim_Woodland: VES_BDU_SS_V_Evolved_Slim
+	{
+		scope=1;
+        uniformclass="53rd_Hephaestus_BDU_SS_Slim_Woodland";
+		hiddenSelectionsTextures[] = 
+        {
+			"V_FZ_Armor\Data\Uniforms\V_BDU_TTsKOWoodland_CO",
+			"V_FZ_Armor\Data\Uniforms\V_BDU_TTsKOWoodland_CO"
         };
 	};
 };
