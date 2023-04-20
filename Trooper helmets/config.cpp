958e417e4e5b2c6f16eb4d748a8fa7b933df641d
Andrew Dean <bodybagfilla@gmail.com>
2023-04-20 00:37:20 -0400

    (no branch): Auto stash before revert of "Helmets"

--- Trooper helmets/config.cpp
+++ Trooper helmets/config.cpp
@@ -7,7 +7,10 @@ class CfgPatches {
         {
 			"53rd_H4_Scout_Helmet",
 			"53rd_EOD_Helmet",
-			"53rd_AA_Helmet",
+			"53rd_AA_Helmet_UB",
+			"53rd_AA_Helmet_WD",
+			"53rd_AA_Helmet_DS",
+			"53rd_AA_Helmet_WN",
 			"53rd_M56SR_Helm",
         };
         requiredVersion = 0.100000;
@@ -24,6 +27,7 @@ class cfgWeapons
 		class MA_M56A_Helmet;
 		class MA_M56E_Helmet;
 		class MA_M56SR_Helmet;
+		class MA_M56SH_Helmet;
 		
 		class 53rd_H4_Scout_Helmet: MA_M56_Scout_Helmet
 		{
@@ -51,26 +55,65 @@ class cfgWeapons
 				"53rd_SC_aux\Trooper helmets\53rd_EOD_Helmet_Visor.paa"
 			};
 		};
-		class 53rd_AA_Helmet:MA_M56A_Helmet
+		class 53rd_AA_Helmet_UB:MA_M56A_Helmet
 		{
 			scope=2;
 			scopeArsenal=2;
-			displayName="[53rd] Air Assault Helmet";
+			displayName="[53rd] Air Assault Helmet (Urban)";
 			model="MA_Armor\data\Helmets\AA\AA_Helmet.p3d";
 			picture="MA_Armor\data\Icons\AA_Helmet.paa"; //TODO: Update this
 				hiddenSelectionsTextures[]=
 				{
-				"53rd_SC_aux\Trooper helmets\53rd_AA_Helmet.paa",
-				"53rd_SC_aux\Trooper helmets\53rd_AA_Helmet_Visor.paa"
+				"53rd_SC_aux\Trooper helmets\AA\[53rd] AA Helmet.paa",
+				"53rd_SC_aux\Trooper helmets\AA\Visor\[53rd] AA Helmet(Visor).paa"
 				};
 		};
-		class 53rd_M56SR_Helmet: MA_M56SR_Helmet
+		class 53rd_AA_Helmet_DS:MA_M56A_Helmet
 		{
 			scope=2;
 			scopeArsenal=2;
-			displayName="[53rd] MSR ODST HR Helmet";
-			model="MA_Armor\data\Helmets\HR_ODST\HR_ODST.p3d";
-			picture="MA_Armor\data\Icons\Halo_Reach_ODST_Helmet.paa";
+			displayName="[53rd] Air Assault Helmet (Desert)";
+			model="MA_Armor\data\Helmets\AA\AA_Helmet.p3d";
+			picture="MA_Armor\data\Icons\AA_Helmet.paa"; //TODO: Update this
+				hiddenSelectionsTextures[]=
+				{
+				"53rd_SC_aux\Trooper helmets\AA\[53rd] AA Helmet (Desert).paa",
+				"53rd_SC_aux\Trooper helmets\AA\Visor\[53rd] AA Helmet(Visor).paa"
+				};
+		};
+		class 53rd_AA_Helmet_WN:MA_M56A_Helmet
+		{
+			scope=2;
+			scopeArsenal=2;
+			displayName="[53rd] Air Assault Helmet (Winter)";
+			model="MA_Armor\data\Helmets\AA\AA_Helmet.p3d";
+			picture="MA_Armor\data\Icons\AA_Helmet.paa"; //TODO: Update this
+				hiddenSelectionsTextures[]=
+				{
+				"53rd_SC_aux\Trooper helmets\AA\[53rd] AA Helmet (Winter).paa",
+				"53rd_SC_aux\Trooper helmets\AA\Visor\[53rd] AA Helmet(Visor_Winter).paa"
+				};
+		};
+		class 53rd_AA_Helmet_WD:MA_M56A_Helmet
+		{
+			scope=2;
+			scopeArsenal=2;
+			displayName="[53rd] Air Assault Helmet (Woodland)";
+			model="MA_Armor\data\Helmets\AA\AA_Helmet.p3d";
+			picture="MA_Armor\data\Icons\AA_Helmet.paa"; //TODO: Update this
+				hiddenSelectionsTextures[]=
+				{
+				"53rd_SC_aux\Trooper helmets\AA\[53rd] AA Helmet (Woodland).paa",
+				"53rd_SC_aux\Trooper helmets\AA\Visor\[53rd] AA Helmet(Visor).paa"
+				};
+		};
+		class 53rd_M56SH_Helmet: MA_M56SH_Helmet
+		{
+			scope=2;
+			scopeArsenal=2;
+			displayName="[53rd] Halo 3 Helmet";
+			model="MA_Armor\data\Helmets\H3_ODST\H3_ODST_Helmet.p3d";
+			picture="MA_Armor\data\Icons\H3_ODST_Helmet.paa";
 			hiddenSelectionsTextures[]=
 			{
 				"53rd_SC_aux\Trooper helmets\53rd_H3_ODST_Helmet.paa",
@@ -78,7 +121,7 @@ class cfgWeapons
 			};
 			class ItemInfo: ItemInfo
 			{
-				uniformModel="MA_Armor\data\Helmets\HR_ODST\HR_ODST.p3d";
+				uniformModel="MA_Armor\data\Helmets\H3_ODST\H3_ODST_Helmet.p3d";
 				hiddenSelectionsTextures[]=
 				{
 					"53rd_SC_aux\Trooper helmets\53rd_H3_ODST_Helmet.paa",
