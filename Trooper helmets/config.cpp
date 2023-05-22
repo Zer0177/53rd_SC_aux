0b88f87aba13520a3d7efcc4cb5bec7401c66f04
Cherryy <Ross_Nichols22@outlook.com>
2023-05-22 16:27:53 -0500

    EOD Helmet All Varients Mk.2 ( I made an oopsie)

--- Trooper helmets/config.cpp
+++ Trooper helmets/config.cpp
@@ -32,19 +32,19 @@
 		class MA_M56E_Helmet;
 		class MA_M56SR_Helmet;
 		class MA_M56SH_Helmet;
-		class 53rd_H4_Scout_Helmet: MA_M56_Scout_Helmet
-		{
-			scope=2;
-			scopeArsenal=2;
-			displayName="[53rd] Scout Helmet";
-			model="MA_Armor\data\Helmets\H4_Scout\H4_Scout_Helmet.p3d";
-			picture="MA_Armor\data\Icons\H4_Scout_Helmet.paa"; //TODO: Update this
-			hiddenSelectionsTextures[]=
-			{
-				"53rd_SC_aux\Trooper helmets\H4\[53rd] H4 Helmet.paa",
-				"53rd_SC_aux\Trooper helmets\H4\Visor\[53rd] H4 Helmet(Visor).paa"
-			};
-		};
+		//class 53rd_H4_Scout_Helmet: MA_M56_Scout_Helmet
+		//{
+			//scope=2;
+			//scopeArsenal=2;
+			//displayName="[53rd] Scout Helmet";
+			//model="MA_Armor\data\Helmets\H4_Scout\H4_Scout_Helmet.p3d";
+			//picture="MA_Armor\data\Icons\H4_Scout_Helmet.paa"; //TODO: Update this
+			//hiddenSelectionsTextures[]=
+			//{
+				//"53rd_SC_aux\Trooper helmets\H4\[53rd] H4 Helmet.paa",
+				//"53rd_SC_aux\Trooper helmets\H4\Visor\[53rd] H4 Helmet(Visor).paa"
+			//};
+		//};
 		class 53rd_EOD_Helmet: MA_M56E_Helmet
 		{
 		scope=2;
@@ -153,44 +153,44 @@
 				"53rd_SC_aux\Trooper helmets\AA\Visor\[53rd] AA Helmet(Visor).paa"
 			};
 		};
-		class 53rd_M56SH_Helmet_UB: MA_M56SH_Helmet
-		{
-			scope=2;
-			scopeArsenal=2;
-			displayName="[53rd] Halo 3 Helmet (Urban)";
-			model="MA_Armor\data\Helmets\H3_ODST\H3_ODST_Helmet.p3d";
-			picture="MA_Armor\data\Icons\H3_ODST_Helmet.paa";
-			hiddenSelectionsTextures[]=
-			{
-				"53rd_SC_aux\Trooper helmets\H3\[53rd] H3_ODST Helmet.paa",
-				"53rd_SC_aux\Trooper helmets\H3\Visor\[53rd] H3_ODST Helmet(Visor).paa"
-			};
-		};
-		class 53rd_M56SH_Helmet_WD: MA_M56SH_Helmet
-		{
-			scope=2;
-			scopeArsenal=2;
-			displayName="[53rd] Halo 3 Helmet (Woodland)";
-			model="MA_Armor\data\Helmets\H3_ODST\H3_ODST_Helmet.p3d";
-			picture="MA_Armor\data\Icons\H3_ODST_Helmet.paa";
-			hiddenSelectionsTextures[]=
-			{
-				"53rd_SC_aux\Trooper helmets\H3\[53rd] H3_ODST Helmet(Woodland).paa",
-				"53rd_SC_aux\Trooper helmets\H3\Visor\[53rd] H3_ODST Helmet(Visor).paa"
-			};
-		};
-		class 53rd_M56SH_Helmet_DS: MA_M56SH_Helmet
-		{
-			scope=2;
-			scopeArsenal=2;
-			displayName="[53rd] Halo 3 Helmet (Desert)";
-			model="MA_Armor\data\Helmets\H3_ODST\H3_ODST_Helmet.p3d";
-			picture="MA_Armor\data\Icons\H3_ODST_Helmet.paa";
-			hiddenSelectionsTextures[]=
-			{
-				"53rd_SC_aux\Trooper helmets\H3\[53rd] H3_ODST Helmet(Desert).paa",
-				"53rd_SC_aux\Trooper helmets\H3\Visor\[53rd] H3_ODST Helmet(Visor).paa"
-			};
+		//class 53rd_M56SH_Helmet_UB: MA_M56SH_Helmet
+		//{
+			//scope=2;
+			//scopeArsenal=2;
+			//displayName="[53rd] Halo 3 Helmet (Urban)";
+			//model="MA_Armor\data\Helmets\H3_ODST\H3_ODST_Helmet.p3d";
+			//picture="MA_Armor\data\Icons\H3_ODST_Helmet.paa";
+			//hiddenSelectionsTextures[]=
+			//{
+				//"53rd_SC_aux\Trooper helmets\H3\[53rd] H3_ODST Helmet.paa",
+				//"53rd_SC_aux\Trooper helmets\H3\Visor\[53rd] H3_ODST Helmet(Visor).paa"
+			//};
+		//};
+		//class 53rd_M56SH_Helmet_WD: MA_M56SH_Helmet
+		//{
+			//scope=2;
+			//scopeArsenal=2;
+			//displayName="[53rd] Halo 3 Helmet (Woodland)";
+			//model="MA_Armor\data\Helmets\H3_ODST\H3_ODST_Helmet.p3d";
+			//picture="MA_Armor\data\Icons\H3_ODST_Helmet.paa";
+			//hiddenSelectionsTextures[]=
+			//{
+				//"53rd_SC_aux\Trooper helmets\H3\[53rd] H3_ODST Helmet(Woodland).paa",
+				//"53rd_SC_aux\Trooper helmets\H3\Visor\[53rd] H3_ODST Helmet(Visor).paa"
+			//};
+		//};
+		//class 53rd_M56SH_Helmet_DS: MA_M56SH_Helmet
+		//{
+			//scope=2;
+			//scopeArsenal=2;
+			//displayName="[53rd] Halo 3 Helmet (Desert)";
+			//model="MA_Armor\data\Helmets\H3_ODST\H3_ODST_Helmet.p3d";
+			//picture="MA_Armor\data\Icons\H3_ODST_Helmet.paa";
+			//hiddenSelectionsTextures[]=
+			//{
+				//"53rd_SC_aux\Trooper helmets\H3\[53rd] H3_ODST Helmet(Desert).paa",
+				//"53rd_SC_aux\Trooper helmets\H3\Visor\[53rd] H3_ODST Helmet(Visor).paa"
+			//};
 
-		};
+		//};
 	};
